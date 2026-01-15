7c67892bde2ebe8edc671f0e6e85577bf3383658
MARVIN <o.silverygold@gmail.com>
2026-01-15 05:46:43 +0300

    Configure kafka producer

---
+++ src/main/java/com/kafka/Orders/config/KafkaProducerConfig.java
@@ -0,0 +1,17 @@
+package com.kafka.Orders.config;
+
+import com.orders.avro.OrderEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaProducerConfig {
+    @Bean
+    public KafkaTemplate<String, OrderEvent> kafkaTemplate(
+            ProducerFactory<String, OrderEvent> producerFactory
+    ){
+        return new KafkaTemplate<String, OrderEvent>(producerFactory);
+    }
+}
