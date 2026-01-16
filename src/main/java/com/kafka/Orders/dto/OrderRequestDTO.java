ee229c43bb40be16de9e20fa3d168f3c4a866357
MARVIN <o.silverygold@gmail.com>
2026-01-16 05:45:11 +0300

    Define data transfer object

---
+++ src/main/java/com/kafka/Orders/dto/OrderRequestDTO.java
@@ -0,0 +1,47 @@
+package com.kafka.Orders.dto;
+
+public class OrderRequestDTO {
+    private String orderId;
+    private String product;
+    private Integer quantity;
+    private Double price;
+
+    public OrderRequestDTO(String orderId, String product, int quantity, double price) {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
