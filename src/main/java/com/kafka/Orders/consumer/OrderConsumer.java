c97d9adcbd444a3f85d1f16d62f51a893a90651f
MARVIN <o.silverygold@gmail.com>
2026-01-18 13:57:44 +0300

    Add retryable topic config on consumer

--- src/main/java/com/kafka/Orders/consumer/OrderConsumer.java
+++ src/main/java/com/kafka/Orders/consumer/OrderConsumer.java
@@ -1,24 +1,34 @@
 package com.kafka.Orders.consumer;
 
 import com.orders.avro.OrderEvent;
+import org.springframework.kafka.annotation.BackOff;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Component;
 import org.springframework.kafka.support.Acknowledgment;
 
 @Component
 public class OrderConsumer {
+    @RetryableTopic(
+            attempts = "3",
+            backOff = @BackOff(delay = 1000, multiplier = 2.0),
+            dltTopicSuffix = ".DLT",
+            retryTopicSuffix = "-RETRY"
+    )
     @KafkaListener(
             topics = "orders",
             containerFactory = "kafkaListenerContainerFactory",
             groupId = "order-consumer-group"
     )
     public void consume(OrderEvent event, Acknowledgment ack){
-        try {
-            process(event);
-            ack.acknowledge();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        if(event.getPrice() <= 0 || event.getQuantity() <=0){
+            throw new IllegalArgumentException(
+                    "Invalid price/quantity { Price: " + event.getPrice()
+                            + " Quantity: " + event.getQuantity() + "}"
+            );
         }
+        process(event);
+        ack.acknowledge();
     }
 
     private void process(OrderEvent event) {
